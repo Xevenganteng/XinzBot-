7d4264176842f60b5a6265dadf51829e142799ce
Xevenganteng <91776917+Xevenganteng@users.noreply.github.com>
2021-11-13 11:53:45 +0700

    Create session.js

---
+++ session.js
@@ -0,0 +1,8 @@
+{
+        "clientID": "4VIIsB4rsMFUh4sTd0iuwg==",
+        "serverToken": "1@E7w+ir+Wh3wktm/brdMnq1V2rnPuUu7LVWxSiawWB8B7yjF3GS/IJRKga2uRT6
+XUsgtR6bPSeIGQWg==",
+        "clientToken": "A6oE5LW+W7NVLxX548cLKtsfJ3+k2T5fOcrBxo5pgJY=",
+        "encKey": "q5liBPNTIGEyOFTYir4Po038FA9Ooug6S0rnSaNz58M=",
+        "macKey": "tZpte5EDBMvPWrB+O4ekeO30cl8FfAs3SrPG9wOCV8Y="
+}
